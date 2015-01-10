db844092396a291a22c284e77c9f81e9dd2e526c
alvaro garcia <derecerca@gmail.com>
2015-01-10 20:41:05 +0100

    refactor: parameter is not needed

--- src/main/java/com/example/kata/rpncalculator/StringParser.java
+++ src/main/java/com/example/kata/rpncalculator/StringParser.java
@@ -6,12 +6,12 @@ package com.example.kata.rpncalculator;
 public class StringParser {
 	public OperationTree parse(String operationChain) {
 		if(operationChain.length() < 8) {
-			return parseSubString(operationChain, 0);
+			return parseSubString(operationChain);
 		} else {
 			return new OperationTree(
 					Expression.operatorFrom(operationChain.charAt(8)),
 					Expression.constant(parseNumber(operationChain.charAt(6))),
-					parseSubString(operationChain, 0));
+					parseSubString(operationChain));
 		}
 	}
 
@@ -19,8 +19,8 @@ public class StringParser {
 		return parseNumber(String.valueOf(character));
 	}
 
-	private OperationTree parseSubString(String operationChain, int begin) {
-		String[] parts = operationChain.substring(begin).split(" +");
+	private OperationTree parseSubString(String operationChain) {
+		String[] parts = operationChain.split(" +");
 
 		return new OperationTree(
 				Expression.operatorFrom(parts[2].charAt(0)),
