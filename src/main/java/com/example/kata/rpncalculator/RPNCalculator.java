174bedde65cd07d364c2f3f8a28405fc1a92027f
alvaro garcia <derecerca@gmail.com>
2015-01-10 14:30:27 +0100

    green

--- src/main/java/com/example/kata/rpncalculator/RPNCalculator.java
+++ src/main/java/com/example/kata/rpncalculator/RPNCalculator.java
@@ -4,11 +4,14 @@ package com.example.kata.rpncalculator;
  * Created by alvaro on 10/01/15.
  */
 public class RPNCalculator {
-	public RPNCalculator(StringParser parserMock) {
+	private final StringParser parser;
 
+	public RPNCalculator(StringParser parserMock) {
+		this.parser = parserMock;
 	}
 
 	public Integer calculate(String operationChain) {
+		parser.parse(operationChain);
 		if ("2 5 +".equals(operationChain)) {
 			return 7;
 		} else {
