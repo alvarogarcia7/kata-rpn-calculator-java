d1f43be5b6668143e28faee25f4cd92a412651a4
alvaro garcia <derecerca@gmail.com>
2015-01-10 14:16:10 +0100

    green

---
+++ src/test/java/com/example/kata/rpncalculator/RPNCalculator.java
@@ -0,0 +1,10 @@
+package com.example.kata.rpncalculator;
+
+/**
+ * Created by alvaro on 10/01/15.
+ */
+public class RPNCalculator {
+	public Integer calculate(String operationChain) {
+		return 7;
+	}
+}
