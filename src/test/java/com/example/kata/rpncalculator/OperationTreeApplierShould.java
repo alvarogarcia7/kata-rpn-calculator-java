8ccb0c12a26306bf523a37f36ed0ca12af3c4aee
alvaro garcia <derecerca@gmail.com>
2015-01-10 15:20:12 +0100

    red: multiply_two_numbers

---
+++ src/test/java/com/example/kata/rpncalculator/OperationTreeApplierShould.java
@@ -0,0 +1,27 @@
+package com.example.kata.rpncalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.example.kata.rpncalculator.Expression.TIMES;
+import static com.example.kata.rpncalculator.OperationTreeBuilder.aNew;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class OperationTreeApplierShould {
+
+	private OperationTreeApplier sut;
+
+	@Before
+	public void setUp() {
+		sut = new OperationTreeApplier();
+	}
+
+	@Test
+	public void multiply_two_numbers() {
+		assertThat(sut.applyOn(aNew().withOperator(TIMES).withOperands(0, 0).build()), is(0));
+	}
+
+
+
+}
