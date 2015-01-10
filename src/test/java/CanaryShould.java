fce5ffe48dc7bcddb888989004bbc90b04239247
alvaro garcia <derecerca@gmail.com>
2015-01-10 14:14:59 +0100

    red: add_two_numbers

--- src/test/java/CanaryShould.java
+++ src/test/java/CanaryShould.java
@@ -7,7 +7,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by alvaro on 10/01/15.
  */
 public class CanaryShould {
-	
+
 	@Test
 	public void work_as_a_canary() {
 		assertThat(true, is(true));
