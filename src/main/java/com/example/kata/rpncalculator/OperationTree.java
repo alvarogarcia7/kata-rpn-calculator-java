dfe4b290b5f6371a284c595e89affc036ebd66ab
alvaro garcia <derecerca@gmail.com>
2015-01-11 02:27:34 +0100

    refactor: implement operationTree based on Computable

--- src/main/java/com/example/kata/rpncalculator/OperationTree.java
+++ src/main/java/com/example/kata/rpncalculator/OperationTree.java
@@ -7,22 +7,19 @@ import java.util.Optional;
  */
 public class OperationTree implements Computable{
 	static final OperationTree EMPTY = null;
-	private final Expression expression1;
 	private final Optional<Operator> operator;
-	private final OperationTree tree2;
-	private final OperationTree tree1;
+	private final Computable tree2;
+	private final Computable tree1;
 
 	public OperationTree(Operator operator, OperationTree tree1, OperationTree tree2) {
 		this.tree1 = tree1;
 		this.tree2 = tree2;
 		this.operator = Optional.of(operator);
-		expression1 = null;
 	}
 
 	public OperationTree(Expression constant) {
-		tree1 = OperationTree.EMPTY;
+		tree1 = constant;
 		tree2 = OperationTree.EMPTY;
-		expression1 = constant;
 		operator = Optional.empty();
 	}
 
@@ -33,7 +30,6 @@ public class OperationTree implements Computable{
 
 		OperationTree that = (OperationTree) o;
 
-		if (expression1 != null ? !expression1.equals(that.expression1) : that.expression1 != null) return false;
 		if (operator != null ? !operator.equals(that.operator) : that.operator != null) return false;
 		if (tree1 != null ? !tree1.equals(that.tree1) : that.tree1 != null) return false;
 		if (tree2 != null ? !tree2.equals(that.tree2) : that.tree2 != null) return false;
@@ -41,25 +37,13 @@ public class OperationTree implements Computable{
 		return true;
 	}
 
-	@Override
-	public int hashCode() {
-		int result = (expression1 != null ? expression1.hashCode() : 0);
-		result = 31 * result + (operator != null ? operator.hashCode() : 0);
-		result = 31 * result + (tree2 != null ? tree2.hashCode() : 0);
-		result = 31 * result + (tree1 != null ? tree1.hashCode() : 0);
-		return result;
-	}
 
 	public Integer compute() {
 		Integer tree1Result;
-		if (expression1 == null) {
-			tree1Result = tree1.compute();
-		} else {
-			tree1Result = expression1.compute();
-		}
+			tree1Result = tree1.calculate();
 
 		if (operator.isPresent()) {
-			Integer tree2Result = tree2.compute();
+			Integer tree2Result = tree2.calculate();
 
 			Operator operator = this.operator.get();
 			if (operator == Operator.TIMES) {
@@ -77,13 +61,26 @@ public class OperationTree implements Computable{
 	}
 
 	@Override
+	public int hashCode() {
+		int result = operator != null ? operator.hashCode() : 0;
+		result = 31 * result + (tree2 != null ? tree2.hashCode() : 0);
+		result = 31 * result + (tree1 != null ? tree1.hashCode() : 0);
+		return result;
+	}
+
+	@Override
 	public String toString() {
-		return "OperationTree{" +
-				 operator +
-				", e1=" + expression1 +
-				", t1=" + tree1 +
-				", t2=" + tree2 +
-				'}';
+		if(operator.isPresent()) {
+			return "{" +
+					operator.get() +
+					", t1=" + tree1 +
+					", t2=" + tree2 +
+					'}';
+		} else {
+			return "{" +
+					tree1 +
+					'}';
+		}
 	}
 
 	@Override
