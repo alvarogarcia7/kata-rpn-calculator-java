8d23ec12b71f125fdf1f72fceb3465c40fb06633
alvaro garcia <derecerca@gmail.com>
2015-01-11 02:06:20 +0100

    refactor: code cleanup

--- src/main/java/com/example/kata/rpncalculator/OperationTree.java
+++ src/main/java/com/example/kata/rpncalculator/OperationTree.java
@@ -7,41 +7,22 @@ import java.util.Optional;
  */
 public class OperationTree {
 	static final OperationTree EMPTY = null;
-	private final Expression expression2;
 	private final Expression expression1;
 	private final Optional<Operator> operator;
 	private final OperationTree tree2;
 	private final OperationTree tree1;
-
-	public OperationTree(Operator operator, Expression expression1, Expression expression2) {
-		this.operator = Optional.of(operator);
-		this.expression1 = expression1;
-		this.expression2 = expression2;
-		this.tree2 = OperationTree.EMPTY;
-		this.tree1 = OperationTree.EMPTY;
-	}
-
-	public OperationTree(Operator operator, Expression operand1, OperationTree firstTree) {
-		this.operator = Optional.of(operator);
-		this.expression1 = operand1;
-		this.expression2 = null;
-		this.tree2 = firstTree;
-		this.tree1 = OperationTree.EMPTY;
-	}
 
 	public OperationTree(Operator operator, OperationTree tree1, OperationTree tree2) {
 		this.tree1 = tree1;
 		this.tree2 = tree2;
 		this.operator = Optional.of(operator);
 		expression1 = null;
-		expression2 = null;
 	}
 
 	public OperationTree(Expression constant) {
 		tree1 = OperationTree.EMPTY;
 		tree2 = OperationTree.EMPTY;
 		expression1 = constant;
-		expression2 = null;
 		operator = Optional.empty();
 	}
 
@@ -53,7 +34,6 @@ public class OperationTree {
 		OperationTree that = (OperationTree) o;
 
 		if (expression1 != null ? !expression1.equals(that.expression1) : that.expression1 != null) return false;
-		if (expression2 != null ? !expression2.equals(that.expression2) : that.expression2 != null) return false;
 		if (operator != null ? !operator.equals(that.operator) : that.operator != null) return false;
 		if (tree1 != null ? !tree1.equals(that.tree1) : that.tree1 != null) return false;
 		if (tree2 != null ? !tree2.equals(that.tree2) : that.tree2 != null) return false;
@@ -63,8 +43,7 @@ public class OperationTree {
 
 	@Override
 	public int hashCode() {
-		int result = expression2 != null ? expression2.hashCode() : 0;
-		result = 31 * result + (expression1 != null ? expression1.hashCode() : 0);
+		int result = (expression1 != null ? expression1.hashCode() : 0);
 		result = 31 * result + (operator != null ? operator.hashCode() : 0);
 		result = 31 * result + (tree2 != null ? tree2.hashCode() : 0);
 		result = 31 * result + (tree1 != null ? tree1.hashCode() : 0);
@@ -75,9 +54,6 @@ public class OperationTree {
 
 		Integer tree2Result = 1;
 		if (tree2 == null) {
-			if(expression2 != null) {
-				tree2Result = expression2.compute();
-			}
 		} else {
 			tree2Result = tree2.compute();
 		}
@@ -111,7 +87,6 @@ public class OperationTree {
 		return "OperationTree{" +
 				 operator +
 				", e1=" + expression1 +
-				", e2=" + expression2 +
 				", t1=" + tree1 +
 				", t2=" + tree2 +
 				'}';
--- src/main/java/com/example/kata/rpncalculator/StringParser.java
+++ src/main/java/com/example/kata/rpncalculator/StringParser.java
@@ -1,6 +1,5 @@
 package com.example.kata.rpncalculator;
 
-import java.util.List;
 import java.util.Stack;
 
 /**
@@ -27,8 +26,6 @@ public class StringParser {
 		}
 
 		return operationStack.pop();
-
-
 	}
 
 	private Expression expression(String current) {
@@ -39,57 +36,8 @@ public class StringParser {
 		return Operator.from(getOperatorFrom(current));
 	}
 
-	private void read(Stack<String> tokenStack, List<OperationTree> operationTrees, List<Expression> expressionList) {
-		if(tokenStack.isEmpty()){
-			return;
-
-		}
-		if(isOperator(tokenStack.peek())){
-			Operator operator = Operator.from(getOperatorFrom(tokenStack.pop()));
-			read(tokenStack, operationTrees, expressionList);
-		} else {
-			Expression operand = getConstantFrom(tokenStack.pop());
-			expressionList.add(operand);
-			read(tokenStack, operationTrees, expressionList);
-		}
-
-	}
-
-
-	private OperationTree popFromList(List<OperationTree> operationTrees) {
-		OperationTree tree1 = operationTrees.get(operationTrees.size() - 1);
-		operationTrees.remove(operationTrees.size() - 1);
-		return tree1;
-	}
-
-	private Stack<String> getTokens(String operationChain) {
-		String[] parts = operationChain.split(" +");
-		Stack<String> tokenStack = new Stack<>();
-		for (String current : parts) {
-			tokenStack.push(current);
-		}
-		return tokenStack;
-	}
-
 	private boolean isOperator(String token) {
 		return Operator.isOne(token);
-	}
-
-	private Operator lastOperator(String operationChain) {
-		return Operator.from(operationChain.charAt(operationChain.length() - 1));
-	}
-
-	private int parseNumber(char character) {
-		return parseNumber(String.valueOf(character));
-	}
-
-	private OperationTree parseSubString(String operationChain, int begin) {
-		String[] parts = operationChain.substring(begin).split(" +");
-
-		return new OperationTree(
-				Operator.from(getOperatorFrom(parts[2])),
-				getConstantFrom(parts[0]),
-				getConstantFrom(parts[1]));
 	}
 
 	private Expression getConstantFrom(String part) {
